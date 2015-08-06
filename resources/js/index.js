edd6235a0cf28738c59bbe51aa98009b755d2d7a
Bryan Elliott <bryanl.elliott419@gmail.com>
2015-08-05 23:01:51 -0400

    #1 added blurred background, more front end work

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
-	$("#navbar").fadeIn(3000);
+	$("#navbar-top").fadeIn(1000);
+	$("#navbar-bottom").fadeIn(1000);
+	$("#site-bground").animate({ opacity: 1 }, { duration: 4000 });
 	// $("#jumboBar").click(function(){
 	// 	this.animate({top: '250px'});
 	// });
+	$("#show-pic").click(function(){
+		$("#site-container").fadeOut(400);
+		$("#show-pic").fadeIn(1);
+	});
 });
